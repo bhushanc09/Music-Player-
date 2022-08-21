832868e374e5167a89ac436f89b973e029b6fb18
bhushanchaudhari <chaudharibhushanj@gmail.com>
2022-08-22 00:40:31 +0530

    Added final

--- src/com/bhushan/musicplayer/musicplayer/songOpertion.java
+++ src/com/bhushan/musicplayer/musicplayer/songOpertion.java
@@ -11,16 +11,24 @@ public class songOpertion {
 
 	public void addSongs() {
 		try {
-		System.out.println("enter no. of song to add");
-		int no = sc.nextInt();
-		for (int i = 0; i < no; i++) {
-			System.out.println("Enter song name:");
-			String s = sc.next();
-			music.add(i, new Song());
-			music.get(i).setId(i + 1);
-			music.get(i).setSongName(s);
-		}
-		}catch(InputMismatchException e) {
+			System.out.println("enter no. of song to add");
+			int no = sc.nextInt();
+			for (int i = 0; i < no; i++) {
+				System.out.println("Enter song name:");
+				String s = sc.next();
+				music.add(i, new Song());
+				music.get(i).setId(i + 1);
+				music.get(i).setSongName(s);
+				System.out.println("Enter movie name:");
+				music.get(i).setMovieName(sc.next());
+				System.out.println("Enter singer name :");
+				music.get(i).setSingerName(sc.next());
+				System.out.println("Enter Composer name:");
+				music.get(i).setComposer(sc.next());
+				System.out.println("Enter song length:");
+				music.get(i).setLength(sc.nextDouble());
+			}
+		} catch (InputMismatchException e) {
 			System.out.println("invalid input");
 		}
 
@@ -29,7 +37,7 @@ public class songOpertion {
 	public void displayListOfSong() {
 
 		if (music.isEmpty() == true) {
-			System.out.println("NO Song In list...!!");
+			System.out.println("NO Song In list...!!");    
 			throw new NullPointerException();
 		} else
 //			System.out.println(music);//
@@ -52,11 +60,9 @@ public class songOpertion {
 			this.displayListOfSong();
 		} catch (NullPointerException e) {
 			System.out.println("No Songs in List. Add some songs first.");
-		}catch(InputMismatchException e) {
+		} catch (InputMismatchException e) {
 			System.out.println("invalid input");
 		}
-
-
 
 	}
 
@@ -72,7 +78,7 @@ public class songOpertion {
 			}
 		} catch (NullPointerException e) {
 			System.out.println("No Songs in List. Add some songs first.");
-		}catch(InputMismatchException e) {
+		} catch (InputMismatchException e) {
 			System.out.println("invalid input");
 		}
 
@@ -90,45 +96,43 @@ public class songOpertion {
 	}
 
 	public void update() {
-		try{
+		try {
 			displayListOfSong();
-		System.out.println("choose song id no");
-		int n = sc.nextInt();
-		System.out.println("1.id \n 2.song name\n 3.movie name \n 4.composer \n 5.length \n 6 back");
-		int key = sc.nextInt();
-		switch (key) {
-		case 1:
-			System.out.println("enter new id");
-			music.get(n - 1).setId(sc.nextInt());
-			break;
-		case 2:
-			System.out.println("enter song by song name");
-			music.get(n - 1).setSongName(sc.next());
-			break;
-		case 3:
-			System.out.println("enter song by movie name");
-			music.get(n - 1).setMovieName(sc.next());
-			break;
-		case 4:
-			System.out.println("enter song by composer");
-			music.get(n - 1).setComposer(sc.next());
-			break;
-		case 5:
-			System.out.println("enter song by song length");
-			music.get(n - 1).setLength(sc.nextDouble());
-			break;
-		case 6:
-			break;
-		}
-		
-		}catch(NullPointerException e) {
-			System.out.println("invalid input");
-		}catch(InputMismatchException e) {
-			System.out.println("invalid input");
-		}
+			System.out.println("choose song id no");
+			int n = sc.nextInt();
+			System.out.println("1.id \n 2.song name\n 3.movie name \n 4.composer \n 5.length \n 6 back");
+			int key = sc.nextInt();
+			switch (key) {
+			case 1:
+				System.out.println("enter new id");
+				music.get(n - 1).setId(sc.nextInt());
+				break;
+			case 2:
+				System.out.println("enter song by song name");
+				music.get(n - 1).setSongName(sc.next());
+				break;
+			case 3:
+				System.out.println("enter song by movie name");
+				music.get(n - 1).setMovieName(sc.next());
+				break;
+			case 4:
+				System.out.println("enter song by composer");
+				music.get(n - 1).setComposer(sc.next());
+				break;
+			case 5:
+				System.out.println("enter song by song length");
+				music.get(n - 1).setLength(sc.nextDouble());
+				break;
+			case 6:
+				break;
+			}
 
+		} catch (NullPointerException e) {
+			System.out.println("invalid input");
+		} catch (InputMismatchException e) {
+			System.out.println("invalid input");
+		}
 
 	}
-		
 
 }
